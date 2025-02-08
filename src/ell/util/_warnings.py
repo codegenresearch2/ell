39e3a80b30ec46236ce72da8f059818089743c43
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-0>
2025-02-08 01:07:51 +0000

    Reset to 13e169ceb5310202abf0cc0768ed1cc1b1b549a1 and made changes for LLM Round 1

--- src/ell/util/_warnings.py
+++ src/ell/util/_warnings.py
@@ -1,2 +1,2 @@
-python\\n    import ell\\n    import openai\\n    \\\n    ell.lm(model, client=openai.Client(api_key=my_key))\\n    def {name}(...):\\n        ...\\n
-python\\n    ell.lm(model, client=openai.Client(api_key=my_key))(...)\\\\\\\\\n
+python\n    import ell\n    import openai\n                                    \n    ell.lm(model, client=openai.Client(api_key=my_key))\n    def {name}(...):\n        ...\n
+python\n    ell.lm(model, client=openai.Client(api_key=my_key))(...)\n
