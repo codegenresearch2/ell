e131cd2a682eb19d28d3f1076d99ff044e59212b
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-09 14:00:45 +0000

    Reset to 576afa20ee8a4312f1004524e46b7f7ceb55f74e and made changes for LLM Round 1

--- src/ell/stores/sql.py
+++ src/ell/stores/sql.py
@@ -1,4 +1,4 @@
-import asyncio
+import datetime
 import json
 import os
 from typing import Any, Optional, Dict, List, Set, Union
@@ -13,19 +13,17 @@ from sqlalchemy import or_, func, and_
 
 
 class SQLStore(ell.store.Store):
-    async def __init__(self, db_uri: str):
-        self.engine = create_engine(db_uri, echo=True)
+    def __init__(self, db_uri: str):
+        self.engine = create_engine(db_uri)
         SQLModel.metadata.create_all(self.engine)
         self.open_files: Dict[str, Dict[str, Any]] = {}
 
-    async def write_lmp(self, lmp_id: str, name: str, source: str, dependencies: List[str], is_lmp: bool, lm_kwargs: str,
-                        version_number: int,
-                        uses: Dict[str, Any],
-                        global_vars: Dict[str, Any],
-                        free_vars: Dict[str, Any],
-                        commit_message: Optional[str] = None,
-                        created_at: Optional[float]=None) -> Optional[Any]:
-        async with Session(self.engine) as session:
+    def write_lmp(self, lmp_id: str, name: str, source: str, dependencies: List[str], is_lmp: bool,
+                  lm_kwargs: str, version_number: int,
+                  uses: Dict[str, Any], global_vars: Dict[str, Any],
+                  free_vars: Dict[str, Any], commit_message: Optional[str] = None,
+                  created_at: Optional[float]=None) -> Optional[Any]:
+        with Session(self.engine) as session:
             lmp = session.query(SerializedLMP).filter(SerializedLMP.lmp_id == lmp_id).first()
             if lmp:
                 # Already added to the DB.
@@ -49,19 +47,18 @@ class SQLStore(ell.store.Store):
                 used_lmp = session.exec(select(SerializedLMP).where(SerializedLMP.lmp_id == use_id)).first()
                 if used_lmp:
                     lmp.uses.append(used_lmp)
-            await session.commit()
+            session.commit()
         return None
 
-    async def write_invocation(self, id: str, lmp_id: str, args: str, kwargs: str, result: Union[lstr, List[lstr]],
-                                invocation_kwargs: Dict[str, Any],
-                                global_vars: Dict[str, Any],
-                                free_vars: Dict[str, Any], created_at: Optional[float],
-                                consumes: Set[str], prompt_tokens: Optional[int] = None,
-                                completion_tokens: Optional[int] = None,
-                                latency_ms: Optional[float] = None,
-                                state_cache_key: Optional[str] = None,
-                                cost_estimate: Optional[float] = None) -> Optional[Any]:
-        async with Session(self.engine) as session:
+    def write_invocation(self, id: str, lmp_id: str, args: str, kwargs: str, result: Union[lstr, List[lstr]],
+                          invocation_kwargs: Dict[str, Any], global_vars: Dict[str, Any],
+                          free_vars: Dict[str, Any], created_at: Optional[float],
+                          consumes: Set[str], prompt_tokens: Optional[int] = None,
+                          completion_tokens: Optional[int] = None,
+                          latency_ms: Optional[float] = None,
+                          state_cache_key: Optional[str] = None,
+                          cost_estimate: Optional[float] = None) -> Optional[Any]:
+        with Session(self.engine) as session:
             if isinstance(result, lstr):
                 results = [result]
             elif isinstance(result, list):
@@ -104,9 +101,12 @@ class SQLStore(ell.store.Store):
                     invocation_consumer_id=id,
                     invocation_consuming_id=consumed_id
                 ))
-            await session.commit()
+            session.commit()
 
-    async def get_latest_lmps(self, skip: int = 0, limit: int = 10) -> List[Dict[str, Any]]:
+    def get_latest_lmps(self, skip: int = 0, limit: int = 10) -> List[Dict[str, Any]]:
+        """
+        Gets all the lmps grouped by unique name with the highest created at
+        """
         subquery = (
             select(SerializedLMP.name, func.max(SerializedLMP.created_at).label("max_created_at"))
             .group_by(SerializedLMP.name)
@@ -116,10 +116,10 @@ class SQLStore(ell.store.Store):
             "name": subquery.c.name,
             "created_at": subquery.c.max_created_at
         }
-        return await self.get_lmps(skip=skip, limit=limit, subquery=subquery, **filters)
+        return self.get_lmps(skip=skip, limit=limit, subquery=subquery, **filters)
 
-    async def get_lmps(self, skip: int = 0, limit: int = 10, subquery=None, **filters: Optional[Dict[str, Any]]) -> List[Dict[str, Any]]:
-        async with Session(self.engine) as session:
+    def get_lmps(self, skip: int = 0, limit: int = 10, subquery=None, **filters: Optional[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        with Session(self.engine) as session:
             query = select(SerializedLMP, SerializedLMPUses.lmp_user_id).outerjoin(
                 SerializedLMPUses,
                 SerializedLMP.lmp_id == SerializedLMPUses.lmp_using_id
@@ -132,17 +132,17 @@ class SQLStore(ell.store.Store):
             if filters:
                 for key, value in filters.items():
                     query = query.where(getattr(SerializedLMP, key) == value)
-            query = query.order_by(SerializedLMP.created_at.desc())  # Sort by created_at in descending order
+            query = query.order_by(SerializedLMP.created_at.desc())
             query = query.offset(skip).limit(limit)
-            results = (await session.execute(query)).all()
+            results = session.exec(query).all()
             lmp_dict = {lmp.lmp_id: {**lmp.model_dump(), 'uses': []} for lmp, _ in results}
             for lmp, using_id in results:
                 if using_id:
                     lmp_dict[lmp.lmp_id]['uses'].append(using_id)
             return list(lmp_dict.values())
 
-    async def get_invocations(self, lmp_filters: Dict[str, Any], skip: int = 0, limit: int = 10, filters: Optional[Dict[str, Any]] = None) -> List[Dict[str, Any]]:
-        async with Session(self.engine) as session:
+    def get_invocations(self, lmp_filters: Dict[str, Any], skip: int = 0, limit: int = 10, filters: Optional[Dict[str, Any]] = None) -> List[Dict[str, Any]]:
+        with Session(self.engine) as session:
             query = select(Invocation, SerializedLStr, SerializedLMP).join(SerializedLMP).outerjoin(SerializedLStr)
             for key, value in lmp_filters.items():
                 query = query.where(getattr(SerializedLMP, key) == value)
@@ -150,7 +150,7 @@ class SQLStore(ell.store.Store):
                 for key, value in filters.items():
                     query = query.where(getattr(Invocation, key) == value)
             query = query.order_by(Invocation.created_at.desc()).offset(skip).limit(limit)
-            results = (await session.execute(query)).all()
+            results = session.exec(query).all()
             invocations = {}
             for inv, lstr, lmp in results:
                 if inv.id not in invocations:
@@ -162,8 +162,8 @@ class SQLStore(ell.store.Store):
                     invocations[inv.id]['results'].append(dict(**lstr.model_dump(), __lstr=True))
             return list(invocations.values())
 
-    async def get_traces(self):
-        async with Session(self.engine) as session:
+    def get_traces(self):
+        with Session(self.engine) as session:
             query = text("""
             SELECT 
                 consumer.lmp_id, 
@@ -176,7 +176,7 @@ class SQLStore(ell.store.Store):
             JOIN 
                 invocation AS consumed ON trace.invocation_consuming_id = consumed.id
             """)
-            results = (await session.execute(query)).all()
+            results = session.exec(query).all()
             traces = []
             for (consumer_lmp_id, consumer_invocation_id, consumed_invocation_id, consumed_lmp_id) in results:
                 traces.append({
@@ -185,8 +185,8 @@ class SQLStore(ell.store.Store):
                 })
             return traces
 
-    async def get_all_traces_leading_to(self, invocation_id: str) -> List[Dict[str, Any]]:
-        async with Session(self.engine) as session:
+    def get_all_traces_leading_to(self, invocation_id: str) -> List[Dict[str, Any]]:
+        with Session(self.engine) as session:
             traces = []
             visited = set()
             queue = [(invocation_id, 0)]
@@ -201,12 +201,12 @@ class SQLStore(ell.store.Store):
 
                 visited.add(current_invocation_id)
 
-                results = (await session.execute(
+                results = session.exec(
                     select(InvocationTrace, Invocation, SerializedLMP)
                     .join(Invocation, InvocationTrace.invocation_consuming_id == Invocation.id)
                     .join(SerializedLMP, Invocation.lmp_id == SerializedLMP.lmp_id)
                     .where(InvocationTrace.invocation_consumer_id == current_invocation_id)
-                )).all()
+                ).all()
                 for row in results:
                     trace = {
                         'consumer_id': row.InvocationTrace.invocation_consumer_id,
@@ -224,7 +224,7 @@ class SQLStore(ell.store.Store):
 
 
 class SQLiteStore(SQLStore):
-    async def __init__(self, storage_dir: str):
+    def __init__(self, storage_dir: str):
         os.makedirs(storage_dir, exist_ok=True)
         db_path = os.path.join(storage_dir, 'ell.db')
         super().__init__(f'sqlite:///{db_path}')
