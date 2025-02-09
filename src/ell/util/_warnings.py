dc153474b08b4ec908633dc365eecbf90af07f74
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-09 13:25:53 +0000

    Reset to 13e169ceb5310202abf0cc0768ed1cc1b1b549a1 and made changes for LLM Round 1

--- src/ell/util/_warnings.py
+++ src/ell/util/_warnings.py
@@ -1,2 +1,22 @@
-python\n    import ell\n    import openai\n                                    \n    ell.lm(model, client=openai.Client(api_key=my_key))\n    def {name}(...):\n        ...\n
-python\n    ell.lm(model, client=openai.Client(api_key=my_key))(...)\n
+import logging
+from colorama import Fore, Style
+from ell.configurator import config
+
+logger = logging.getLogger(__name__)
+
+
+def _no_api_key_warning(model, name, client_to_use, long=False, error=False):
+    color = Fore.RED if error else Fore.LIGHTYELLOW_EX
+    prefix = 'ERROR' if error else 'WARNING'
+    return f'{color}{prefix}: No API key found for model `{model}` used by LMP `{name}` using client `{client_to_use}`' + (f'\n\nTo fix this:' if long else ' at time of definition. Can be okay if custom client specified later! <TODO: add link to docs> ') + f'{Style.RESET_ALL}'
+
+
+def _warnings(model, fn, default_client_from_decorator):
+    if not default_client_from_decorator:
+        client_to_use = config.model_registry.get(model, None)
+        if not client_to_use:
+            logger.warning(
+                _no_api_key_warning(model, fn.__name__, client_to_use, long=True)
+            )
+        elif not client_to_use.api_key:
+            logger.warning(_no_api_key_warning(model, fn.__name__, client_to_use, long=False))
