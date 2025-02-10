5f04f18a25798beaa49da307258e007ad3c2ca85
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 18:34:26 +0000

    Reset to f6878115246a9f42cdfdfd7b98b4e2ff31fbdbec and made changes for LLM Round 1

--- src/ell/store.py
+++ src/ell/store.py
@@ -4,6 +4,7 @@ from typing import Any, Optional, Dict, List, Set
 from ell._lstr import _lstr
 from ell.types import SerializedLMP, Invocation
 from ell.types.message import InvocableLM
+from datetime import datetime
 
 class Store(ABC):
     """
@@ -25,12 +26,11 @@ class Store(ABC):
         pass
 
     @abstractmethod
-    def write_invocation(self, invocation: Invocation, results: List[_lstr], consumes: Set[str]) -> Optional[Any]:
+    def write_invocation(self, invocation: Invocation, consumes: Set[str]) -> Optional[Any]:
         """
         Write an invocation of an LMP to the storage.
 
         :param invocation: Invocation object containing all invocation details.
-        :param results: List of SerializedLStr objects representing the results.
         :param consumes: Set of invocation IDs consumed by this invocation.
         :return: Optional return value.
         """
