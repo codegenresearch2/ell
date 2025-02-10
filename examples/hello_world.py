e73701a0da0617b848c61018a28aacb8f8a75f6e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 00:31:03 +0000

    Reset to 4bc861f1c19022d4e498d836fb401f3c83ef1356 and made changes for LLM Round 1

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,26 +1,19 @@
 import ell
 import numpy as np
-
 from ell.stores.sql import SQLiteStore
 
+def get_random_length():
+    return int(np.random.beta(2, 6) * 3000)
 
-def run_conversations():
-    ell.config.verbose = True
-    ell.set_store('./logdir', autocommit=True)
-
-    def get_random_length():
-        return int(np.random.beta(2, 6) * 3000)
-
-    @ell.simple(model="gpt-4o-mini")
-    def hello(world: str):
-        """Your goal is to be really meant to the other guy while saying hello"""
-        name = world.capitalize()
-        number_of_chars_in_name = get_random_length()
-        return f"Say hello to {name} in {number_of_chars_in_name} characters or more!"
-
-    greeting = hello("sam altman")  # > "hello sama! ... "
-    print(greeting.split(" ")[-1])
-
+@ell.simple(model="gpt-4o-mini")
+def hello(world: str):
+    """Your goal is to be really meant to the other guy while saying hello"""
+    name = world.capitalize()
+    number_of_chars_in_name = get_random_length()
+    return f"Say hello to {name} in {number_of_chars_in_name} characters or more!"
 
 if __name__ == "__main__":
-    run_conversations()
+    ell.config.verbose = True
+    ell.set_store(SQLiteStore('./logdir'), autocommit=True)
+    greeting = hello("sam altman")  # > "hello sama! ... "
+    print(greeting.split(" ")[-1])
