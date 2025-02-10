ee4bae9a34b5688e9227e76d5cfad316ab1e5721
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 23:12:36 +0000

    Reset to 96b95d62d7848bba49f8f9d1f030f2adc9d793a0 and made changes for LLM Round 1

--- src/ell/__init__.py
+++ src/ell/__init__.py
@@ -9,18 +9,3 @@ import ell.models
 
 # Import everything from configurator
 from ell.configurator import *
-
-# Remove unnecessary imports
-# from ell.lmp.simple import simple
-# from ell.lmp.tool import tool
-# from ell.lmp.complex import complex
-# from ell.types.message import system, user, assistant
-# from ell.__version__ import __version__
-
-# Remove the Documentation class and instantiation
-# class Documentation(BaseModel):
-#     description: str
-
-# Remove print statements and JSON dumping
-# documentation = Documentation(description="ell is a Python library for language model programming (LMP). It provides a simple and intuitive interface for working with large language models.")
-# print(documentation.model_dump_json(indent=4))
