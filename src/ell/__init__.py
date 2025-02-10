010b6e2d4355a029c32de9cb2609fb9127a88d1e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 23:27:47 +0000

    Reset to 96b95d62d7848bba49f8f9d1f030f2adc9d793a0 and made changes for LLM Round 1

--- src/ell/__init__.py
+++ src/ell/__init__.py
@@ -1,3 +1,8 @@
+"""
+ell is a Python library for language model programming (LMP). It provides a simple
+and intuitive interface for working with large language models.
+"""
+
 from ell.lmp.simple import simple
 from ell.lmp.tool import tool
 from ell.lmp.complex import complex
@@ -7,5 +12,5 @@ from ell.__version__ import __version__
 # Import all models
 import ell.models
 
-# Import everything from configurator
-from ell.configurator import *
+# Import specific classes from ell.types.message
+from ell.types.message import Message, ContentBlock
