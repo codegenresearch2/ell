2d529195339044615b02cb36c99765b9cafbe87c
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 23:52:56 +0000

    Reset to 96b95d62d7848bba49f8f9d1f030f2adc9d793a0 and made changes for LLM Round 1

--- src/ell/__init__.py
+++ src/ell/__init__.py
@@ -3,15 +3,27 @@ ell is a Python library for language model programming (LMP). It provides a simp
 and intuitive interface for working with large language models.
 """
 
+# Importing the version of the ell library
+from ell.__version__ import __version__
+
+# Importing BaseModel for serialization
 from pydantic import BaseModel
+
+# Importing necessary classes from ell.lmp
 from ell.lmp.simple import simple
 from ell.lmp.tool import tool
 from ell.lmp.complex import complex
-from ell.types.message import system, user, assistant, Message, ContentBlock
-import ell.models  # Importing models from ell.models
-from ell import __version__ as ell_version  # Importing the version of the ell library
-from ell.configurator import *  # Importing everything from ell.configurator
 
-# For more information about ell, visit: https://github.com/microsoft/ell
-# For more information about BaseModel, visit: https://docs.pydantic.dev/usage/models/
-# For more information about ell.types.message, visit: https://github.com/microsoft/ell/blob/main/ell/types/message.py
+# Importing necessary classes from ell.types.message
+from ell.types.message import system, user, assistant, Message, ContentBlock
+
+# Importing all models from ell.models
+import ell.models
+
+# Importing everything from ell.configurator
+from ell.configurator import *
+
+# External links for more information
+# ell: https://github.com/microsoft/ell
+# BaseModel: https://docs.pydantic.dev/usage/models/
+# ell.types.message: https://github.com/microsoft/ell/blob/main/ell/types/message.py
