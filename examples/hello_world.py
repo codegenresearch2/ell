c73a1dfedc81b128376073be6d5fa39162fcc25d
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 00:16:53 +0000

    Reset to 4bc861f1c19022d4e498d836fb401f3c83ef1356 and made changes for LLM Round 1

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,34 +1,26 @@
 import ell
 import numpy as np
 
-
-
-
-
 from ell.stores.sql import SQLiteStore
 
 
-ell.config.verbose = True
-ell.set_store('./logdir', autocommit=True)
-# equivalent to
-# ell.init(store='./logdir', autocommit=True, verbose=True)
+def run_conversations():
+    ell.config.verbose = True
+    ell.set_store('./logdir', autocommit=True)
 
+    def get_random_length():
+        return int(np.random.beta(2, 6) * 3000)
 
-def get_random_length():
-    return int(np.random.beta(2, 6) * 3000)
+    @ell.simple(model="gpt-4o-mini")
+    def hello(world: str):
+        """Your goal is to be really meant to the other guy while saying hello"""
+        name = world.capitalize()
+        number_of_chars_in_name = get_random_length()
+        return f"Say hello to {name} in {number_of_chars_in_name} characters or more!"
 
-@ell.simple(model="gpt-4o-mini")
-def hello(world : str):
-    """Your goal is to be really meant to the other guy while sayi hello"""
-    name = world.capitalize()
-    number_of_chars_in_name = get_random_length()
-
-    return f"Say hello to {name} in {number_of_chars_in_name} characters or more!"
-
+    greeting = hello("sam altman")  # > "hello sama! ... "
+    print(greeting.split(" ")[-1])
 
 
 if __name__ == "__main__":
-    greeting = hello("sam altman") # > "hello sama! ... "
-
-    # List of strings
-    print(greeting.split(" ")[-1])
+    run_conversations()
