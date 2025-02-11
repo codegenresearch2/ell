40ba48b8ba231a5e89cc91ed6a13a07575aed6ab
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 07:25:09 +0000

    Reset to 4bc861f1c19022d4e498d836fb401f3c83ef1356 and made changes for LLM Round 1

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -3,25 +3,23 @@ import numpy as np
 
 from ell.stores.sql import SQLiteStore
 
-def initialize_store():
-    ell.config.verbose = True
-    ell.set_store('./logdir', autocommit=True)
-
-def calculate_random_length():
+def get_random_length():
     return int(np.random.beta(2, 6) * 3000)
 
 @ell.simple(model="gpt-4o-mini")
-def generate_greeting(name: str):
+def hello(world: str):
     """Your goal is to be really kind to the person while saying hello"""
-    capitalized_name = name.capitalize()
-    char_limit = calculate_random_length()
+    name = world.capitalize()
+    number_of_chars_in_name = get_random_length()
 
-    return f"Say hello to {capitalized_name} in {char_limit} characters or more!"
+    return f"Say hello to {name} in {number_of_chars_in_name} characters or more!"
 
 if __name__ == "__main__":
-    initialize_store()
-    greeting = generate_greeting("sam altman")
+    ell.config.verbose = True
+    ell.set_store('./logdir', autocommit=True)
 
-    # Extract the last word from the greeting
+    greeting = hello("sam altman")
+
+    # List of strings
     last_word = greeting.split(" ")[-1]
-    print(last_word)
+    print([last_word])
