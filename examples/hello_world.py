f53d2c268c91234ba04eee4124e60acc131d9c53
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 11:48:31 +0000

    Reset to 4bc861f1c19022d4e498d836fb401f3c83ef1356 and made changes for LLM Round 1

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,29 +1,22 @@
 import ell
 import numpy as np
-from typing import List
+from ell.stores.sql import SQLiteStore
 
-ell.init(store='./logdir', autocommit=True, verbose=True)
+ell.config.verbose = True
+ell.set_store(SQLiteStore('./logdir'), autocommit=True)
 
-def get_random_length() -> int:
+def get_random_length():
     return int(np.random.beta(2, 6) * 3000)
 
 @ell.simple(model="gpt-4o-mini")
-def generate_greeting(name: str) -> str:
+def hello(world: str):
     """Your goal is to be really meant to the other guy while saying hello"""
-    capitalized_name = name.capitalize()
-    char_limit = get_random_length()
+    name = world.capitalize()
+    number_of_chars_in_name = get_random_length()
 
-    return f"Say hello to {capitalized_name} in {char_limit} characters or more!"
+    return f"Say hello to {name} in {number_of_chars_in_name} characters or more!"
 
 if __name__ == "__main__":
-    while True:
-        user_input = input("Enter a name (or 'exit' to quit): ")
-        if user_input.lower() == 'exit':
-            break
-
-        greeting = generate_greeting(user_input)
-        greeting_words = greeting.split(" ")
-        last_word = greeting_words[-1]
-
-        print(f"Generated greeting: {greeting}")
-        print(f"Last word of the greeting: {last_word}")
+    greeting = hello("sam altman")
+    last_word = greeting.split()[-1]
+    print(f"Last word of the greeting: {last_word}")
