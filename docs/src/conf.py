b6dcf379686031c4a5d1972c639ef899a5b52f01
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 15:52:41 +0000

    Reset to 96b95d62d7848bba49f8f9d1f030f2adc9d793a0 and made changes for LLM Round 1

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -16,9 +16,6 @@ author = 'William Guss'
 # Correct the extension name
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinxawesome_theme']
 
-# Specify the templates path
-templates_path = ['_templates']
-
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = "sphinxawesome_theme"
@@ -48,3 +45,6 @@ html_theme_options = {
     "logo_light": "_static/ell-wide-light.png",
     "logo_dark": "_static/ell-wide-dark.png",
 }
+
+# Specify the templates path
+templates_path = ['_templates']
