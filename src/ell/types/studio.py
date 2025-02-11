64340e9e94a76fd6eeb70b6f742225f04d4ee64a
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 10:46:16 +0000

    Reset to 96b95d62d7848bba49f8f9d1f030f2adc9d793a0 and made changes for LLM Round 1

--- src/ell/types/studio.py
+++ src/ell/types/studio.py
@@ -1,13 +1,8 @@
-
-# In ell/types/studio.py
 # Before:
 # from ell.types import SerializedLMP
 
 # After:
-from ell.types.datamodels import SerializedLMP
-
-# If SerializedLMP is defined in a different module, adjust the import statement accordingly.
-# For example, if it's defined in ell/types/lmp.py:
-# from ell.types.lmp import SerializedLMP
+# Assuming SerializedLMP is defined in ell/types/lmp.py
+from ell.types.lmp import SerializedLMP
 
 # Now, the SerializedLMP class should be accessible from the ell/types/studio.py module.
