84a235aeadf1d5ea397e7646a8de04ba7b1d5504
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 07:21:20 +0000

    Reset to 4bc861f1c19022d4e498d836fb401f3c83ef1356 and made changes for LLM Round 1

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -3,24 +3,20 @@ import numpy as np
 
 from ell.stores.sql import SQLiteStore
 
+# Initialize ell configuration
+ell.config.verbose = True
+ell.set_store(SQLiteStore('./logdir'), autocommit=True)
 
 def get_random_length():
     return int(np.random.beta(2, 6) * 3000)
 
 @ell.simple(model="gpt-4o-mini")
 def hello(world: str):
-    """Your goal is to be really meant to the other guy while saying hello"""
+    """Says hello to the given world in a specified number of characters."""
     name = world.capitalize()
     number_of_chars_in_name = get_random_length()
-
     return f"Say hello to {name} in {number_of_chars_in_name} characters or more!"
 
-
 if __name__ == "__main__":
-    ell.config.verbose = True
-    ell.set_store('./logdir', autocommit=True)
-
     greeting = hello("sam altman")  # > "hello sama! ... "
-
-    # List of strings
     print(greeting.split(" ")[-1])
